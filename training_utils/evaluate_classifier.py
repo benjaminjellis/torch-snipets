47fad36749ef756119dc6a5d1df88ab27c57ef00
Benjamin Ellis <benellisuk@gmail.com>
2021-03-27 00:16:45 +0000

    added logger and precison + recall

--- training_utils/evaluate_classifier.py
+++ training_utils/evaluate_classifier.py
@@ -1,117 +1,61 @@
+"""
+Evaluation function for top-1, top-k, f1 score, precision and recall
+"""
 import torch
-import torch.optim as optim
-from torch.optim.lr_scheduler import OneCycleLR
-from optimisers import SAM
-from efficientnet_pytorch import EfficientNet
-from losses import smooth_cross_entropy
-from trainer.utils import *
-
+import torch.nn as nn
 from torch.utils.data import DataLoader
-from trainer import evaluate_model
-
-from traaining_logger import TrainLogger
-from tqdm import tqdm
+from sklearn.metrics import f1_score, precision_score, recall_score
 from typing import Optional
 
 
-def train(experiment_no: int,
-          train_dataloader: DataLoader,
-          val_dataloader: DataLoader,
-          lr: float,
-          epochs: int,
-          rho: float,
-          model_name: str,
-          num_classes: int,
-          params_to_train: Optional[int] = None,
-          k: Optional[int] = None
-          ) -> None:
+def evaluate_model(val_dataloader: DataLoader,
+                   device: str,
+                   model: nn.Module,
+                   k: Optional[int] = None,
+                   logger = None) -> tuple:
     """
-    Trainer for efficient nets using SAM + SGD with once cycle lr schedule
-    :param experiment_no: uid for saving outputs into log and saving model checkpoint
+    Evaluate model on a data loader
     :param val_dataloader: validation dataloader
-    :param train_dataloader: train dataloader
-    :param lr: learning rate
-    :param rho: rho param for SAM
-    :param epochs: number of epochs to train for
-    :param model_name: model name of efficient net to use
-    :param num_classes: number of prediction classes
-    :param params_to_train: Optional - number of layers to train, if 1 - just the top layer
-    :param k: Optional - top-k for evaluation (default is 5)
-    :return: nothing
+    :param device: cuda or cpu
+    :param model: nn.Module
+    :param k: for top-k accuracy
+    :param logger: logger
+    :return: top-1 accuracy, top-k accuracy, f1_score, precision, recall
     """
-    log = TrainLogger(experiment_type = "exp", experiment_no = experiment_no)
+    if not k:
+        k = 5
 
-    # log hyperparams
-    log.log_hyperparameters(lr = lr, rho = rho, epochs = epochs,
-                            batch_size = train_dataloader.batch_size,
-                            model_name = model_name,
-                            params_to_train = params_to_train)
+    predictions_stacked = []
+    targets_stacked = []
 
-    torch.manual_seed(0)
+    correct_top_1 = 0
+    correct_top_k = 0
+    total = 0
 
-    model = EfficientNet.from_pretrained(model_name, num_classes = num_classes)
-
-    device = ("cuda" if torch.cuda.is_available() else "cpu")
-
-    if params_to_train:
-        # set only the last params_to_train params to be trainable
-        count = 0
-        for param in model.parameters():
-
-            if 341 - count > params_to_train + 1:
-                param.requires_grad = False
-            count += 1
-
-    model = model.to(device)
-
-    trainable_params = [p for p in model.parameters() if p.requires_grad]
-
-    base_optimizer = optim.SGD
-
-    optimizer = SAM(trainable_params,
-                    base_optimizer,
-                    rho = rho,
-                    lr = lr,
-                    momentum = 0.9,
-                    weight_decay = 1e-5)
-
-    lr_sched = OneCycleLR(optimizer = optimizer,
-                          max_lr = lr,
-                          epochs = epochs,
-                          steps_per_epoch = len(train_dataloader))
-
-    log.log_hyperparameters(optimizer = type(optimizer).__name__,
-                            lr_sched = type(lr_sched).__name__)
-
-    for epoch in range(epochs):
-        train_dataloader = tqdm(train_dataloader)
-        running_loss = 0
-        for i, data in enumerate(train_dataloader):
-            model.train()
+    with torch.no_grad():
+        model.eval()
+        for data in val_dataloader:
             images, labels = (d.to(device) for d in data)
-
-            # first forward-backward step
             outputs = model(images)
-            loss = smooth_cross_entropy(outputs, labels)
-            loss.mean().backward()
-            optimizer.first_step(zero_grad = True)
+            preds_top_1 = torch.argmax(outputs, 1)
+            preds_top_k = torch.topk(outputs, k, dim = 1)[1]
+            total += outputs.shape[0]
+            correct_top_1 += (preds_top_1 == labels).sum().item()
 
-            # second forward-backward step
-            outputs2 = model(images)
-            loss2 = smooth_cross_entropy(outputs2, labels)
-            loss2.mean().backward()
-            optimizer.second_step(zero_grad = True)
+            for i in range(preds_top_k.shape[0]):
+                if labels[i] in preds_top_k[i]:
+                    correct_top_k += 1
 
-            running_loss = loss.mean() + loss2.mean()
+            targets_stacked += labels.tolist()
+            predictions_stacked += preds_top_1.tolist()
 
-            lr_sched.step()
-
-        train_result = f"Train loss after {epoch + 1} epochs: {running_loss}"
-        print(train_result)
-        log.log_result(train_result)
-
-        save_checkpoint(model = model, experiment_type = "exp", experiment_number = experiment_no,
-                        epoch = epoch)
-
-        evaluate_model(val_dataloader = val_dataloader, device = device, model = model,
-                       k = k, logger = log)
+    top_1_acc = correct_top_1 / total * 100
+    top_k_acc = correct_top_k / total * 100
+    val_f1_score = f1_score(targets_stacked, predictions_stacked, average = "micro")
+    val_precision = precision_score(targets_stacked, predictions_stacked, average = "micro")
+    val_recall = recall_score(targets_stacked, predictions_stacked, average = "micro")
+    val_result = f"Val | top-1 acc: {top_1_acc} | top-{k} acc: {top_k_acc} | f1 score: {val_f1_score} | precision {val_precision} | recall {val_recall}"
+    print(val_result)
+    if logger:
+        logger.log_result(val_result)
+    return top_1_acc, top_k_acc, val_f1_score, val_precision, val_recall
